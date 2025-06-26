a6cf36f17ed2e5735349496850bbace739748e1f
Maryuum-yusuf <maryamayusuf690@gmail.com>
2025-06-27 00:23:23 +0300

    Fix database init for Gunicorn

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ load_dotenv()
 
 app = Flask(__name__)
 
-db_path = os.path.join(os.path.dirname(__file__), "translations.db")
+db_path = os.path.join(os.getcwd(), "translations.db")
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
